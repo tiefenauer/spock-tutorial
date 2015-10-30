9162c458a318d6da8d79ad05c64e3d39a6aa38e1
Daniel Tiefenauer <daniel.tiefenauer@bbv.ch>
2015-10-30 10:06:21 +0100

    adding Radio and RadioStation from JBehave tutorial

---
+++ src/main/java/info/tiefenauer/tutorials/spock/Radio.java
@@ -0,0 +1,57 @@
+package info.tiefenauer.tutorials.spock;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by danieltiefenauer on 30.10.2015.
+ */
+public class Radio {
+
+    private String stationName;
+    private boolean turnedOn;
+    private String currentStation;
+    private String currentFrequency;
+    private List<RadioStation> presets = new ArrayList<>();
+
+    public Radio() {
+
+    }
+
+    public void switchOnOff(){
+        turnedOn = !turnedOn;
+    }
+
+    public boolean isTurnedOn() {
+        return turnedOn;
+    }
+
+    public void selectStation(String stationName) {
+        currentStation = stationName;
+    }
+
+    public String getDisplay() {
+        return getShortName(currentStation);
+    }
+
+    private String getShortName(String currentStation) {
+        if (currentStation == null)
+            return "";
+        if (currentStation.length() > 10)
+            return currentStation.substring(0,7) + "...";
+        return currentStation;
+    }
+
+    public void tuneTo(double frequency) {
+        currentFrequency = new DecimalFormat("#.00").format(frequency) + " FM";
+    }
+
+    public String getCurrentFrequency() {
+        return currentFrequency;
+    }
+
+    public void addPreset(RadioStation station) {
+        presets.add(station);
+    }
+}
---
+++ src/main/java/info/tiefenauer/tutorials/spock/RadioStation.java
@@ -0,0 +1,31 @@
+package info.tiefenauer.tutorials.spock;
+
+/**
+ * Created by danieltiefenauer on 30.10.2015.
+ */
+public class RadioStation {
+
+    private String _name;
+    private double _frequency;
+
+    public RadioStation(String name, String frequency) {
+        setName(name);
+        setFrequency(Double.valueOf(frequency));
+    }
+
+    public double getFrequency() {
+        return _frequency;
+    }
+
+    public void setFrequency(double frequency) {
+        _frequency = frequency;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+}
